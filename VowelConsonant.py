f7623b0dfe19bad2a6411a74b296167ca538fc8a
sharmilasubramanian <sharmila_subramanian@srmuniv.edu.in>
2017-07-15 14:50:19 +0530

    Add files via upload

---
+++ VowelConsonant.py
@@ -0,0 +1,14 @@
+n=raw_input()
+if n=="e" or n=="E":
+	print "VOWEL"
+elif n=="a"or n=="A":
+	print "VOWEL"
+elif n=="i" or n=="I":
+	print "VOWEL"
+elif n=="o"or n=="O":
+	print "Vowel"
+elif n=="u" or n=="U":
+	print "Vowel"
+else:
+    print "Consonant"
+    
